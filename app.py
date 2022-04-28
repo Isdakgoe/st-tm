2e78c5c1d002b088f17ca721f773b98ae99bcbcc
isdakgoe <taka.goe4@gmail.com>
2022-04-28 13:35:22 +0900

    Merge branch 'master' of https://github.com/Isdakgoe/st-tm
     Conflicts:
    	app.py

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import pandas as pd
 import matplotlib.pyplot
 import streamlit as st
 
+def git_setting():
+    print("PUSHED")
+    # git init
+    # heroku git:remote -a st-tm
+    # git add .
+    # git commit -am "コメント"
+    # git push heroku master
+    # git remote add heroku https://git.heroku.com/st-tm.git
+
 
 st.title("Visualizing Central Limit Theorem")
 
