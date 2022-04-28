ba55486926e535010bde574569f154a0d2af51c7
isdakgoe <taka.goe4@gmail.com>
2022-04-28 13:17:16 +0900

    コメント

---
+++ app.py
@@ -0,0 +1,42 @@
+
+import pandas as pd
+import matplotlib.pyplot
+import streamlit as st
+
+
+st.title("Visualizing Central Limit Theorem")
+
+"""
+heroku login
+heroku create st-tm
+
+git init
+heroku git:remote -a st-tm
+git add .
+git commit -am "コメント"
+git push heroku master
+git remote add heroku https://git.heroku.com/st-tm.git
+
+
+# git init
+# git add .
+# git commit -m "first commit"
+# heroku create st-tm
+# git push heroku master
+# heroku open
+# heroku logs --tail
+
+git init
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Isdakgoe/st-tm.git
+git push -u origin main
+"""
+
+
+# heroku create st-tm
+
+# git push heroku master
+# heroku open
+# heroku logs --tail
+
