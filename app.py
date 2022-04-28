bd9346ca1e9fea74513c14994671a3977481e700
isdakgoe <taka.goe4@gmail.com>
2022-04-28 14:52:43 +0900

    Merge branch 'master' of https://github.com/Isdakgoe/st-tm
     Conflicts:
    	app.py

--- app.py
+++ app.py
@@ -26,9 +26,6 @@ class StreamlitTM:
         self.db = pd.read_csv(path_db, encoding="utf_8_sig")
 
     def chose_extract_player(self):
-        self.db_use = self.db.copy()
-        self.rule = ""
-
         self.PitcherTeam = self._extract_values_to_chose_widget(w_No=0, col="PitcherTeam")
         self.Pitcher = self._extract_values_to_chose_widget(w_No=1, col="Pitcher")
         self.BatterTeam = self._extract_values_to_chose_widget(w_No=2, col="BatterTeam")
@@ -37,22 +34,15 @@ class StreamlitTM:
         self.btn_table_show = st.button("Show")
 
     def _extract_values_to_chose_widget(self, w_No, col):
-        col_values_set = sorted(set(self.db_use[col]))
+        col_values_set = sorted(set(self.db[col]))
         value_out = self.wid_cols[w_No].selectbox(col, col_values_set, index=0)
-
-        self.db_use.query(f'{col} == "{value_out}"', inplace=True)
-
-        rule_out = f'{col} == "{value_out}"'
-        if self.rule == "":
-            self.rule = rule_out
-        else:
-            self.rule = self.rule + " & " + rule_out
         return value_out
 
     def fnc_show_table(self):
+        self.rule = "Pitcher == @self.Pitcher & Batter == @self.Batter"
         st.write(self.rule)
         if self.btn_table_show:
-            st.write(self.db_use)
+            st.write(self.db.query(self.rule))
 
 
 if __name__ == '__main__':
