a658d1c10aadb525f32767abd7ec8dfbaff05b23
isdakgoe <taka.goe4@gmail.com>
2022-04-28 14:49:15 +0900

    Merge branch 'master' of https://github.com/Isdakgoe/st-tm
     Conflicts:
    	app.py

--- app.py
+++ app.py
@@ -18,30 +18,46 @@ class StreamlitTM:
     def __init__(self):
         # setting streamlit
         st.set_page_config(layout="wide")
-        st.title("Visualizing Central Limit Theorem")
-        self.wid_cols = st.beta_columns(3)
+        st.title("TrackMan DashBoard")
+        self.wid_cols = st.beta_columns(4)
 
         # read data
-        path_db_summary = 'templates/DB_PitchType_NPB_2022.csv'
-        self.db_summary = pd.read_csv(path_db_summary, encoding="utf-8-sig")
+        path_db = 'templates/tm_2022_db.csv'
+        self.db = pd.read_csv(path_db, encoding="utf_8_sig")
 
     def chose_extract_player(self):
-        PitcherTeam_list = sorted(set(self.db_summary["PlayerTeam"]))
-        PlayerTeam = self.wid_cols[0].selectbox("PlayerTeam", PitcherTeam_list, index=0)
+        self.db_use = self.db.copy()
+        self.rule = ""
 
-        Pitcher_list = sorted(set(self.db_summary.query('PlayerTeam == @PlayerTeam')["Player"]))
-        self.Player = self.wid_cols[1].selectbox("Player", Pitcher_list, index=0)
+        self.PitcherTeam = self._extract_values_to_chose_widget(w_No=0, col="PitcherTeam")
+        self.Pitcher = self._extract_values_to_chose_widget(w_No=1, col="Pitcher")
+        self.BatterTeam = self._extract_values_to_chose_widget(w_No=2, col="BatterTeam")
+        self.Batter = self._extract_values_to_chose_widget(w_No=3, col="Batter")
 
-        self.btn_table_show = self.wid_cols[2].button("Show")
+        self.btn_table_show = st.button("Show")
+
+    def _extract_values_to_chose_widget(self, w_No, col):
+        col_values_set = sorted(set(self.db_use[col]))
+        value_out = self.wid_cols[w_No].selectbox(col, col_values_set, index=0)
+
+        self.db_use.query(f'{col} == "{value_out}"', inplace=True)
+
+        rule_out = f'{col} == "{value_out}"'
+        if self.rule == "":
+            self.rule = rule_out
+        else:
+            self.rule = self.rule + " & " + rule_out
+        return value_out
 
     def fnc_show_table(self):
+        st.write(self.rule)
         if self.btn_table_show:
-            db_summary_show = self.db_summary.query('Player == @self.Player')
-            st.write(db_summary_show)
+            st.write(self.db_use)
 
 
-self = StreamlitTM()
-self.chose_extract_player()
-self.fnc_show_table()
+if __name__ == '__main__':
+    self = StreamlitTM()
+    self.chose_extract_player()
+    self.fnc_show_table()
 
-# if __name__ == '__main__':
+# streamlit run app.py
