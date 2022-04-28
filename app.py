bd0f5fa998310470d1dbc40da9f7930960c0ec5a
isdakgoe <taka.goe4@gmail.com>
2022-04-28 13:48:23 +0900

    Merge branch 'master' of https://github.com/Isdakgoe/st-tm
     Conflicts:
    	app.py

--- app.py
+++ app.py
@@ -19,13 +19,14 @@ st.title("Visualizing Central Limit Theorem")
 path_db_summary = 'templates/DB_PitchType_NPB_2022.csv'
 db_summary = pd.read_csv(path_db_summary, encoding="utf-8-sig")
 
+wid_cols = st.beta_columns(3)
 PitcherTeam_list = sorted(set(db_summary["PlayerTeam"]))
-PlayerTeam = st.selectbox("PlayerTeam", PitcherTeam_list, index=0)
+PlayerTeam = wid_cols[0].selectbox("PlayerTeam", PitcherTeam_list, index=0)
 
 Pitcher_list = sorted(set(db_summary.query('PlayerTeam == @PlayerTeam')["Player"]))
-Player = st.selectbox("Player", Pitcher_list, index=0)
+Player = wid_cols[1].selectbox("Player", Pitcher_list, index=0)
 
-btn_table_show = st.button("Show")
+btn_table_show = wid_cols[2].button("Show")
 if btn_table_show:
     db_summary_show = db_summary.query('Player == @Player')
     st.write(db_summary_show)
